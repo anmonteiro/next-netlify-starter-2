6a74fee967d18c406dd6ac25c53bf2ac6a28ea1f
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2022-02-25 11:16:14 -0800

    deploy

--- f/hello.js
+++ f/hello.js
@@ -3,6 +3,6 @@ exports.handler = async function(event, context) {
   console.log('ev', event);
   return {
     statusCode: 200,
-    body: JSON.stringify({event: event})
+    body: JSON.stringify({event: event, context: context, env: process.env})
   }
 }
